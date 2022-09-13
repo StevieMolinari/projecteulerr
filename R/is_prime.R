8262f583bda8305cc66c70fff507473d6b3ba943
StephenMolinari <stephen.molinari@gmail.com>
2022-09-13 14:44:41 -0600

    adds `is_primes`

---
+++ R/is_prime.R
@@ -0,0 +1,18 @@
+is_prime <- function(n){
+    if(round(n) != n){
+        stop("`n` must be an integer.")}
+    if(n < 0){
+        stop("`n` must be a positive integer.")}
+
+    ## 1 is neither prime nor composite
+    if(n == 1){
+        return(NA)}
+
+    if(n %in% c(2, 3)){
+        return(TRUE)}
+
+    ## all numbers less than sqrt(n) are not factors
+    return(
+        all(n %% 2:floor(sqrt(n)) != 0))
+}
+
