e3ea416437342be9024a985336e32bb7b7fa5c0a
StephenMolinari <stephen.molinari@gmail.com>
2023-05-26 09:57:13 -0600

    edited/added get_fibonaccis... functions, documentation, and tests

--- R/get_fibonaccis.R
+++ R/get_fibonaccis.R
@@ -1,19 +1,4 @@
-#' Provides first n fibonacci numbers
-#'
-#' @param n the number of fibonacci numbers to return
-#' @param firstTwo the first two number in the sequence (default is `c(1, 1)`)
-#'
-#' @return a numeric of length n containing the sequence of fibonacci numbers
-#'
-#' @seealso [get_nth_fibonacci()]
-#'
-#' @examples
-#' get_first_n_fibonaccis(1)
-#' get_first_n_fibonaccis(2)
-#' get_first_n_fibonaccis(5)
-#'
-#' # Lucas numbers
-#' get_first_n_fibonaccis(5, firstTwo = c(1, 3))
+#' @rdname get_fibonaccis
 #'
 #' @export
 
@@ -44,6 +29,78 @@ check_firstTwo_argument <- function(firstTwo){
     }
 }
 
+#' @rdname get_fibonaccis
+#'
+#' @export
+get_fibonaccis_up_to <- function(bound){
+    check_non_negative_integer(bound)
+    if(bound == 0){return(integer())}
+    if(bound == 1){return(c(1L, 1L))}
+    n = ceiling(
+        log(bound*sqrt(5)-1)/log((1+sqrt(5))/2)
+    )
+    fibonaccis = get_first_n_fibonaccis(n)
+    return(fibonaccis[fibonaccis <= bound])
+}
+
+#' Provides a sequence of Fibonacci numbers
+#'
+#' @param n the number of terms in the sequence
+#' @param bound the upper bound for the terms in the sequence
+#' @param ... further arguments to be passed to `get_first_n_fibonaccis` or `get_fibonaccis_up_to`
+#'
+#' @return a sequence of integers
+#'
+#' @seealso [get_nth_fibonacci()]
+#'
+#' @section May be useful for:
+#' * Problem 2: Even Fibonacci numbers
+#'
+#' @examples
+#' get_fibonaccis_up_to(10)
+#' get_first_n_fibonaccis(10)
+#'
+#' # `firstTwo` parameter can be used to generate other sequences:
+#'
+#' # Lucas numbers
+#' get_first_n_fibonaccis(10, firstTwo = c(1, 3))
+#'
+#' # one negative and one positive can be fun to play with
+#' get_first_n_fibonaccis(10, firstTwo = c(1, -1))
+#' get_first_n_fibonaccis(10, firstTwo = c(-9, 6))
+#' get_first_n_fibonaccis(10, firstTwo = c(-8, 5))
+#'
+#'
+#' # in `get_fibonaccis` default behavior is to prefer `n`
+#' get_fibonaccis(10)
+#'
+#' # make explicit call to bound for use of `get_fibonaccis_up_to`
+#' get_fibonaccis(bound=10)
+#'
+#' # when both are supplied and `bound` is ignored and a warning is thrown
+#' get_fibonaccis(n=10, bound=10)
+#'
+#' @export
+get_fibonaccis <- function(n, bound, ...){
+    if(missing(n) & missing(bound)){
+        stop("Either `n` or `bound` argument must be provided.")
+    }
+    if(missing(n)){
+        return(get_fibonaccis_up_to(bound))
+    } else{
+        if(!missing(bound)){
+            warning(
+                "`n` and `bound` arguments supplied; ",
+                "`bound` argument is ignored"
+            )
+        }
+        return(get_first_n_fibonaccis(n, ...))
+    }
+}
+
+
+
+
 #' Provides nth fibonnacci number
 #'
 #' Uses explicit closed-form formula to compute nth fibonacci number.
@@ -51,8 +108,10 @@ check_firstTwo_argument <- function(firstTwo){
 #' @param n the fibonnaci number returned
 #'
 #' @return a single numeric
-#' @seealso [get_nth_fibonacci()]
+#' @seealso [get_fibonaccis()]
 #'
+#' @section May be useful for:
+#' * Problem 25: 1000-digit Fibonacci number
 #'
 #' @examples
 #' get_nth_fibonacci(5)
@@ -67,17 +126,3 @@ get_nth_fibonacci <- function(n){
         (((1+sqrt(5))/2)^n - ((1-sqrt(5))/2)^n) / sqrt(5)
     ))
 }
-
-
-# get_fibonaccis_up_to <- function(maxVal){
-#     # check_non_negative(maxVal)
-#     check_non_negative_integer(maxVal)
-#     if(maxVal < 1){return(numeric())}
-#     if(maxVal < 2){return(c(1L, 1L))}
-#     phi = (1+sqrt(5))/2
-#     n = max(0, ceiling(
-#         log((maxVal-1)*sqrt(5)/log(phi))
-#     )) + 2
-#     fibonaccis = get_first_n_fibonaccis(n)
-#     return(fibonaccis[fibonaccis <= maxVal])
-# }
--- tests/testthat/test-get_fibonaccis.R
+++ tests/testthat/test-get_fibonaccis.R
@@ -57,22 +57,51 @@ test_that("`get_first_n_fibonaccis` and `get_nth_fibonacci` agree", {
     expect_identical(get_first_n_fibonaccis(5), sapply(1:5, get_nth_fibonacci))
 })
 
-# test_that("`get_fibonaccis_up_to` has valid `upTo` argument", {
-#     expect_error(get_fibonaccis_up_to(upTo = "abc"))
-#     expect_error(get_fibonaccis_up_to(upTo = -2))
-# })
-#
-# test_that("`get_fibonaccis_up_to` returns correct sequence", {
-#     expect_identical(get_fibonaccis_up_to(upTo = 0), numeric(0))
-#     # expect_identical(get_fibonaccis_up_to(upTo = 1/2), numeric(0))
-#     expect_identical(get_fibonaccis_up_to(upTo = 1), c(1L, 1L))
-#     # expect_identical(get_fibonaccis_up_to(upTo = 3/2), c(1L, 1L))
-#     expect_identical(get_fibonaccis_up_to(upTo = 2), c(1L, 1L, 2L))
-#     expect_identical(get_fibonaccis_up_to(upTo = 3), c(1L, 1L, 2L, 3L))
-#     expect_identical(get_fibonaccis_up_to(upTo = 4), c(1L, 1L, 2L, 3L))
-#     expect_identical(get_fibonaccis_up_to(upTo = 5), c(1L, 1L, 2L, 3L, 5L))
-#     expect_identical(
-#         get_fibonaccis_up_to(upTo = 100),
-#         get_first_n_fibonaccis(11)
-#     )
-# })
+test_that("`get_fibonaccis_up_to` has valid `bound` argument", {
+    expect_error(get_fibonaccis_up_to(bound = "abc"))
+    expect_error(get_fibonaccis_up_to(bound = -2))
+})
+
+test_that("`get_fibonaccis_up_to` returns correct sequence", {
+    expect_identical(get_fibonaccis_up_to(bound = 0), integer(0))
+    expect_identical(get_fibonaccis_up_to(bound = 1), c(1L, 1L))
+    expect_identical(get_fibonaccis_up_to(bound = 2), c(1L, 1L, 2L))
+    expect_identical(get_fibonaccis_up_to(bound = 3), c(1L, 1L, 2L, 3L))
+    expect_identical(get_fibonaccis_up_to(bound = 4), c(1L, 1L, 2L, 3L))
+    expect_identical(get_fibonaccis_up_to(bound = 5), c(1L, 1L, 2L, 3L, 5L))
+    expect_identical(
+        get_fibonaccis_up_to(bound = 100),
+        get_first_n_fibonaccis(11)
+    )
+})
+
+test_that("`get_fibonaccis` has arguments", {
+    expect_error(get_fibonaccis(), "argument must be provided")
+})
+
+
+test_that("`get_fibonaccis` provides correct values with `bound` argument", {
+    expect_identical(get_fibonaccis(bound=4), c(1L, 1L, 2L, 3L))
+})
+
+test_that("`get_fibonaccis`'s provides correct values with `n` argument", {
+    expect_identical(get_fibonaccis(n=4), c(1L, 1L, 2L, 3L))
+})
+
+test_that("`get_fibonaccis`'s has correct behavior when `n` and bound arguments provided", {
+    expect_warning(
+        get_fibonaccis(n=4, bound=10),
+        "`n` and `bound` arguments supplied"
+    )
+    expect_identical(
+        suppressWarnings(get_fibonaccis(n=4, bound=10)),
+        c(1L, 1L, 2L, 3L)
+    )
+})
+
+test_that("`get_fibonaccis`'s provides correct values with `n` and `firstTwo` arguments", {
+    expect_identical(
+        get_fibonaccis(n=4, firstTwo=c(1L, 3L)),
+        c(1L, 3L, 4L, 7L)
+    )
+})
